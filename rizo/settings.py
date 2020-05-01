615bceabe2dcc4eafae9355c624560d361f7d3a5
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-05-01 13:12:39 +0530

    whitenoise

--- rizo/settings.py
+++ rizo/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url 
