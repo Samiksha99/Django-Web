b82b30a029e673f3be588e86c905f18c44002bfd
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-05-08 19:57:31 +0530

    database name

--- rizo/settings.py
+++ rizo/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'rizo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.d17juu14of7kbv',
-        'NAME':'rizochem',
+        'NAME':'d17juu14of7kbv',
         'USER':'mlcntxocdrnxvo',
         'PASSWORD':'13ed078cc436033b71bcfbff2e7ce0ad85ac6eb57089da291ecd83042c183dae',
         'HOST':'ec2-3-216-129-140.compute-1.amazonaws.com',
